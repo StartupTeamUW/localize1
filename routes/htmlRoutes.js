1c33219d95a1d634831f2017eeaf698dededf466
kimkablitz <kim.tran549@gmail.com>
2018-11-27 19:52:34 -0800

    trip-start-end

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/trip", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+      res.render("trip", {
+        // msg: "Welcome!",
+        // examples: dbExamples
       });
     });
   });
