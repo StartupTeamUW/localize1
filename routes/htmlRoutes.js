18d729621495f608b5c6055c0d89d7dfac9700a4
Kevin <kphyde@gmail.com>
2018-11-30 14:41:07 -0800

    create homepage.hbs in this branch and cut styles over to static css file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,16 @@ module.exports = function (app) {
       });
     });
   });
+
+  app.get("/homepage", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("homepage", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+  
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
