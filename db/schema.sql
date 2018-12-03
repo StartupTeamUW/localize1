33e6e6f3d6c8046755af2c3ec9f81c28187ae45f
kimkablitz <kim.tran549@gmail.com>
2018-12-02 20:44:29 -0800

    restored test and tripdb of schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,11 @@
 DROP DATABASE IF EXISTS localize_db;
 -- Creates the database --
 CREATE DATABASE localize_db;
+
+
+--Testdb -- should be removed when 'examples' is gone
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
+
+DROP DATABASE IF EXISTS tripdb;
+CREATE DATABASE tripdb;
