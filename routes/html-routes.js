f86e0ae01a37efd68c5e69dd378d0a4bed0da641
kcwjy90 <kcwjy90@gmail.com>
2018-12-05 22:00:55 -0800

    not now

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -29,7 +29,7 @@ module.exports = function (app) {
       res.render("kevinUserDetailedPage", {
         msg: "Users Card",
         user: dbUser,
-        interests: dbUser.interests
+        // interests: dbUser.interests
       });
     });
   });
