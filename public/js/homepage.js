1cd4fa830a71c5cceaf4b5c976184845f991fcdb
kimkablitz <kim.tran549@gmail.com>
2018-12-05 21:58:59 -0800

    kevin's slacked over homepage new design

---
+++ public/js/homepage.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    window.onscroll = function () { scrollFunction() };
+  
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-70px";
+      }
+    }
+  });
