98f3e99663c51580bd0749c7c4fa34ec042df9ca
kimkablitz <kim.tran549@gmail.com>
2018-12-08 20:42:54 -0800

    added config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "localize_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "uqhk1tq7qnr0ngh0",
+    "password": "lb4jmte9854iz8us",
+    "database": "kz7wbftgh63cmdmj",
+    "host": "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql"
+  }
+}
