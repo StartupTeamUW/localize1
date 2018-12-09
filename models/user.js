1a1a9a3092d20ac902c4efc51667d569cc5a7a6e
kimkablitz <kim.tran549@gmail.com>
2018-12-09 02:00:03 -0800

    small changes

--- models/user.js
+++ models/user.js
@@ -3,37 +3,37 @@ module.exports = function (sequelize, DataTypes) {
     //Signup Required
     user_name: {
       type: DataTypes.STRING,
-      // allowNull: false,
-      // unique: true, // user name can't be dupclicated!
-      // validate: {
-      //   len: {
-      //     args: 3,
-      //     msg: "User name must be at least 3 characters in length"
-      //   }
-      // }
+      allowNull: false,
+      unique: true, // user name can't be dupclicated!
+      validate: {
+        len: {
+          args: 3,
+          msg: "User name must be at least 3 characters in length"
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
-      // allowNull: false,
-      // unique: true,
-      // validate: {
-      //   len: {
-      //     args: [6, 128],
-      //     msg: "Email address must be between 6 and 128 characters in length"
-      //   },
-      //   isEmail: {
-      //     msg: "Email address must be valid"
-      //   }
-      // }
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [6, 128],
+          msg: "Email address must be between 6 and 128 characters in length"
+        },
+        isEmail: {
+          msg: "Email address must be valid"
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      // allowNull: false,
-      // validate: {
-      //   len: {
-      //     args: 3
-      //   }
-      // }
+      allowNull: false,
+      validate: {
+        len: {
+          args: 3
+        }
+      }
     },
     phonenumber: {
       type: DataTypes.STRING,
@@ -52,10 +52,10 @@ module.exports = function (sequelize, DataTypes) {
     hometown: DataTypes.STRING,
     DOB: DataTypes.STRING,
     bio: {
-      type: DataTypes.TEXT
-      // validate: {
-      //   len: [1, 300]
-      // }
+      type: DataTypes.TEXT,
+      validate: {
+        len: [1, 300]
+      }
     },
     whyOn:{
       type: DataTypes.STRING
